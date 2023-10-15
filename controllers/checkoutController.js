8727a9a83f04617e0f9268ea476fcc9c7391b60a
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-10-15 19:15:59 +0530

    Update checkoutController.js

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,55 +1,114 @@
+ // const Product = require('../models/productModel');
+
+// const processCheckout = async (req, res) => {
+//   try {
+//     // TODO: Fetch all products from the database
+//     // const products = await Product.find();
+
+//     // TODO: Check if any products were found, if not, send a 404 response
+//     // if (!products || products.length === 0) {
+//     //   return res.status(404).json({ message: 'No products found' });
+//     // }
+
+//     let shouldProceedToCheckout = false;
+
+//     // TODO: Loop through the products and check if quantity is greater than 1
+//     // for (const product of products) {
+//     //   if (product.quantity > 1) {
+//     //     shouldProceedToCheckout = true;
+//     //     break;
+//     //   }
+//     // }
+
+//     // TODO: If shouldProceedToCheckout is false, send a response indicating no products with quantity > 1
+//     // if (!shouldProceedToCheckout) {
+//     //   return res.status(400).json({
+//     //     message: 'No products with quantity > 1. Cannot proceed to checkout.',
+//     //   });
+//     // }
+
+//     // TODO: Calculate the total cost by looping through products and applying pricing logic
+//     // let totalCost = 0;
+//     // for (const product of products) {
+//     //   // Consider your pricing logic, for simplicity, let's assume price * quantity
+//     //   totalCost += product.price * product.quantity;
+//     // }
+
+//     // TODO: Apply a discount if the total cost is more than a certain amount
+//     // if (totalCost > 100) {
+//     //   const discountAmount = 0.2 * totalCost; // 20% discount
+//     //   totalCost -= discountAmount;
+//     // }
+
+//     // TODO: Respond with a payment confirmation, total cost, and product details
+//     // For simplicity, we'll respond with a payment confirmation
+//     // res.status(200).json({
+//     //   message: 'Payment successful',
+//     //   totalCost,
+//     //   products,
+//     // });
+//   } catch (error) {
+//     // Handle any errors and send an error response
+//     // res.status(500).json({ message: 'Internal server error', error });
+//   }
+// };
+
+// module.exports = {
+//   processCheckout,
+// };
+
 const Product = require('../models/productModel');
 
 const processCheckout = async (req, res) => {
   try {
-    // TODO: Fetch all products from the database
-    // const products = await Product.find();
+    // Fetch all products from the database
+    const products = await Product.find();
 
-    // TODO: Check if any products were found, if not, send a 404 response
-    // if (!products || products.length === 0) {
-    //   return res.status(404).json({ message: 'No products found' });
-    // }
+    // Check if any products were found, if not, send a 404 response
+    if (!products || products.length === 0) {
+      return res.status(404).json({ message: 'No products found' });
+    }
 
     let shouldProceedToCheckout = false;
 
-    // TODO: Loop through the products and check if quantity is greater than 1
-    // for (const product of products) {
-    //   if (product.quantity > 1) {
-    //     shouldProceedToCheckout = true;
-    //     break;
-    //   }
-    // }
+    // Loop through the products and check if quantity is greater than 1
+    for (const product of products) {
+      if (product.quantity > 1) {
+        shouldProceedToCheckout = true;
+        break;
+      }
+    }
 
-    // TODO: If shouldProceedToCheckout is false, send a response indicating no products with quantity > 1
-    // if (!shouldProceedToCheckout) {
-    //   return res.status(400).json({
-    //     message: 'No products with quantity > 1. Cannot proceed to checkout.',
-    //   });
-    // }
+    // If shouldProceedToCheckout is false, send a response indicating no products with quantity > 1
+    if (!shouldProceedToCheckout) {
+      return res.status(400).json({
+        message: 'No products with quantity > 1. Cannot proceed to checkout.',
+      });
+    }
 
-    // TODO: Calculate the total cost by looping through products and applying pricing logic
-    // let totalCost = 0;
-    // for (const product of products) {
-    //   // Consider your pricing logic, for simplicity, let's assume price * quantity
-    //   totalCost += product.price * product.quantity;
-    // }
+    // Calculate the total cost by looping through products and applying pricing logic
+    let totalCost = 0;
+    for (const product of products) {
+      // Consider your pricing logic, for simplicity, let's assume price * quantity
+      totalCost += product.price * product.quantity;
+    }
 
-    // TODO: Apply a discount if the total cost is more than a certain amount
-    // if (totalCost > 100) {
-    //   const discountAmount = 0.2 * totalCost; // 20% discount
-    //   totalCost -= discountAmount;
-    // }
+    // Apply a discount if the total cost is more than a certain amount
+    if (totalCost > 100) {
+      const discountAmount = 0.2 * totalCost; // 20% discount
+      totalCost -= discountAmount;
+    }
 
-    // TODO: Respond with a payment confirmation, total cost, and product details
+    // Respond with a payment confirmation, total cost, and product details
     // For simplicity, we'll respond with a payment confirmation
-    // res.status(200).json({
-    //   message: 'Payment successful',
-    //   totalCost,
-    //   products,
-    // });
+    res.status(200).json({
+      message: 'Payment successful',
+      totalCost,
+      products,
+    });
   } catch (error) {
     // Handle any errors and send an error response
-    // res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: 'Internal server error', error: error.message });
   }
 };
 
